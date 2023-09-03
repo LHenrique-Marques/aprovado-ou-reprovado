3abbe34ac8e8a4d959ff53ea86177f1400814d8d
Henrique Marques <henrique.marquesvj@gmail.com>
2023-09-03 17:02:48 -0300

    concluindo tarefa

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+///Com Switch
+$nota = 10;
+switch ($nota) {
+    case $nota >= 7:
+        $mensagem = "Parabéns você foi aprovado!";
+        break;
+    
+    case $nota <= 5:
+        $mensagem = "Você está Reprovado!";
+        break;
+    case $nota < 7 and $nota > 5:
+        $mensagem = "Você está em recuperação!";
+        break;
+    
+    default:
+        $mensagem = "Você não tirou uma nota valida.";
+        break;
+}
+echo $mensagem . "<hr>";
+///Com if e else
+$nota2 = 8;
+
+
+if ($nota2 >= 7) {
+    $mensagem_if = "Parabéns você foi aprovado!"
+}
+
+elseif ($nota2 <=5) {
+    $mensagem_if = "Você está Reprovado!"
+}
+
+elseif ($nota2 <7 and $nota >5) {
+    $mensagem_if = "Você está em Recuperação!"
+}
+?>
